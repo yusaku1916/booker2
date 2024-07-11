3a5a93ad88ef455d11d9e501a2e18173f38cd8dd
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2024-07-11 16:39:01 +0000

    [Start]Book関係のファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one_attached :profile_image
+  has_many :boolsks, dependent: :destroy
   
   def get_profile_image
     unless profile_image.attached?

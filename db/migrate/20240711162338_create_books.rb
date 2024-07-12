8191962b3d0c2fa99e26ae753b66082cd2e4465e
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2024-07-12 08:57:20 +0000

    [Modify]Bookモデル

--- db/migrate/20240711162338_create_books.rb
+++ db/migrate/20240711162338_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :body
-      t.integer :uaer_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2024_07_11_162338) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "uaer_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

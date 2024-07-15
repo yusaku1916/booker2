83075725bd0a4dc0e432fe36d2f6de74629e89d6
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2024-07-15 15:00:08 +0000

    [Add]Edit.Destroy実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,26 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.find(current_user.id)
   end
-  
+
   def show
     @books = Book.where(user_id: current_user.id)
     @user = User.find(current_user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to books_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 end

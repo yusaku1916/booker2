4cb4d44ce576b668ad72e836d4fd2e3bdbf90782
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2024-07-17 14:38:31 +0000

    [Start]バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
   
+  validates :name, presence: true, length: {in:2..10}
+  validates :introduction, length: {maximum: 50}
+  
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
